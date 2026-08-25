441266de42c1cff92310819b05966ecfafb3a853
Tur1ddu <orlandosalvatore294@gmail.com>
2026-08-25 22:27:19 +0200

    esercizio 8

--- 8-condizioni-MEDIO.py
+++ 8-condizioni-MEDIO.py
@@ -6,3 +6,15 @@ classificazione di un numero
 - stampare messaggi appropriati, ad esempio "Il numero è positivo e pari", "Il numero è negativo e dispari", o "Il numero è zero"
 
 """
+numero = int(input("inserire numero intero\n"))
+
+if numero == 0:
+    print ("il numero è zero")
+elif numero < 0 and numero %2 == 0:
+    print ("il numero è negativo e pari")
+elif numero < 0 and numero %2 != 0:
+    print ("il numero è negativo e dispari")
+elif numero > 0 and numero %2 == 0:
+    print("il numero è positivo e pari")
+else:
+    print("il numero è positivo e dipari")
