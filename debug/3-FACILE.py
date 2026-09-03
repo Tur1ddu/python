c8a170daa1e4713912d229a889c652efd8bed84d
Tur1ddu <orlandosalvatore294@gmail.com>
2026-09-03 22:52:03 +0200

    esercizio debug 3

--- debug/3-FACILE.py
+++ debug/3-FACILE.py
@@ -1,7 +1,7 @@
 #La funzione deve restituire True se il numero è pari, False se è dispari. Esempio: is_pari(4) → True, is_pari(7) → False
 
 def is_pari(n):
-    return n // 2 == 0
+    return n % 2 == 0
 
 print(is_pari(4))
 print(is_pari(7))
