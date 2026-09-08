a0651910ea8bec02a315aef43ed84fd298a75a54
Tur1ddu <orlandosalvatore294@gmail.com>
2026-09-08 19:11:21 +0200

    esercizio debug 15

--- debug/15-MEDIO.py
+++ debug/15-MEDIO.py
@@ -1,6 +1,6 @@
 #Stampare la tabellina di un numero scelto
 n = int(input("Quale tabellina vuoi vedere? "))
 
-for i in range(10):
+for i in range(1,11):
     prodotto = n * i
     print(f"{n} x {i} = {prodotto}")
